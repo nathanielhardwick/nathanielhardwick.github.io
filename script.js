9663bd34d6ad2dbad8ec4926ba0d3aa63928eb42
nathanielhardwick <nathanielhardwick@gmail.com>
2026-02-05 14:56:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.slider-container');
-    const beforeWrapper = document.querySelector('.image-before-wrapper');
+    const wrapper = document.querySelector('.image-before-wrapper');
     const handle = document.querySelector('.slider-handle');
+    const beforeImage = document.querySelector('.image-before');
     
-    // Check if elements exist (in case you haven't added images yet)
-    if(container && beforeWrapper && handle) {
-        
-        container.addEventListener('mousemove', (e) => {
-            let containerWidth = container.offsetWidth;
-            let x = e.pageX - container.offsetLeft;
-            
-            // Boundary checks
-            if (x < 0) x = 0;
-            if (x > containerWidth) x = containerWidth;
-            
-            // Move the "Before" image wrapper width
-            let percentage = (x / containerWidth) * 100;
-            beforeWrapper.style.width = percentage + "%";
-            
-            // Move the handle
-            handle.style.left = percentage + "%";
-        });
-        
-        // Touch support for mobile
-        container.addEventListener('touchmove', (e) => {
-            let containerWidth = container.offsetWidth;
-            let x = e.touches[0].pageX - container.offsetLeft;
-            
-            if (x < 0) x = 0;
-            if (x > containerWidth) x = containerWidth;
-            
-            let percentage = (x / containerWidth) * 100;
-            beforeWrapper.style.width = percentage + "%";
-            handle.style.left = percentage + "%";
-        });
+    function syncImageWidth() {
+        if(container && beforeImage) {
+            beforeImage.style.width = `${container.offsetWidth}px`;
+        }
     }
+
+    syncImageWidth();
+    window.addEventListener('resize', syncImageWidth);
+
+    let isDragging = false;
+
+    // Start Drag
+    container.addEventListener('mousedown', () => isDragging = true);
+    container.addEventListener('touchstart', () => isDragging = true);
+
+    // Stop Drag
+    window.addEventListener('mouseup', () => isDragging = false);
+    window.addEventListener('touchend', () => isDragging = false);
+
+    // Move Logic
+    function move(e) {
+        if (!isDragging) return;
+        
+        let clientX = e.clientX;
+        if(e.touches && e.touches.length > 0) {
+            clientX = e.touches[0].clientX;
+        }
+        
+        const rect = container.getBoundingClientRect();
+        let x = clientX - rect.left;
+        
+        if (x < 0) x = 0;
+        if (x > rect.width) x = rect.width;
+        
+        const percentage = (x / rect.width) * 100;
+        
+        wrapper.style.width = `${percentage}%`;
+        handle.style.left = `${percentage}%`;
+    }
+
+    container.addEventListener('mousemove', move);
+    container.addEventListener('touchmove', move);
 });
